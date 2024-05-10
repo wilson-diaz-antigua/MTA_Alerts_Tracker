9ad37f7694675d7fd0fb64e62490c9831d3753e6
Wilson <wilson.diant@gmail.com>
2024-05-09 20:42:04 -0400

    tailwind added

---
+++ app-client/src/pages/index.js
@@ -0,0 +1,14 @@
+function Index() {
+  return (
+    <div className="-my-6">
+      <div className="timelineItem group">
+        <div className=" group-last:before:hidden verticalLine">
+          <time className="leftInfo">May, 2020</time>
+          <div className="rightInfo"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Index;

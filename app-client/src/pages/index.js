6a47299d7279cfa172ea6181fee4665a98c2fa94
Wilson <wilson.diant@gmail.com>
2024-05-14 07:27:55 -0400

    refactor and remove unused files

--- app-client/src/pages/index.js
+++ app-client/src/pages/index.js
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
 function Index() {
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/stops")
+      .then((res) => res.json())
+      .then((data) => {
+        setdata(data);
+      });
+  }, []);
+
   return (
     <div className="-my-6">
-      <div className="timelineItem group">
-        <div className=" group-last:before:hidden verticalLine">
-          <time className="leftInfo">May, 2020</time>
-          <div className="rightInfo"></div>
-        </div>
-      </div>
+      {data.map((stop) => {
+        return (
+          <div key={stop.id} className="timelineItem group">
+            <div className=" group-last:before:hidden verticalLine">
+              <div className="rightInfo">{stop.stop}</div>
+            </div>
+            {stop.alert.map((alert, index) => {
+              return (
+                <div key={index}>
+                  <ul className="leftInfo">
+                    <li className="items">{alert.route}</li>
+                  </ul>
+                </div>
+              );
+            })}
+            {console.log(stop.alert)}
+            {stop.alert.map((alert, index) => {
+              return (
+                <div key={index}>
+                  <div div className="">
+                    <div className="text-slate-500">{alert.alert_type}</div>
+                    <div className="text-slate-500">{alert.heading}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }

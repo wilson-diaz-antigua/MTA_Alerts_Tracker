c5bc930f74f6990569be8a9f177b467b5f7e8a18
Wilson <wilson.diant@gmail.com>
2024-12-15 17:08:43 -0500

    refactor: Update database.py to use Path for environment file and add error handling for missing POSTGRES_USER

--- backend/services/database.py
+++ backend/services/database.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from dotenv import dotenv_values
 from flask import Flask
@@ -6,17 +7,23 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from sqlmodel import SQLModel, create_engine
 
-vals = dotenv_values(".env.postgres")
-KEY = os.getenv("SUPABASE_API_KEY")
+env_path = Path(__file__).parent.parent / ".env.postgres"
+
+vals = dotenv_values(env_path)
 # from backend.route import server
+print(vals)
 user = vals["POSTGRES_USER"]
 password = vals["POSTGRES_PASSWORD"]
 host = vals["POSTGRES_HOST"]
 port = vals["POSTGRES_PORT"]
 database = vals["POSTGRES_DB"]
+if not user:
+    raise KeyError("POSTGRES_USER environment variable is not set")
 connection_str = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
 # The engine is the interface to our database so we can execute SQL commands
 engine = create_engine(connection_str)
+if not user:
+    raise KeyError("POSTGRES_USER environment variable is not set")
 
 
 def init_db():

b7bb188ba9867341f7cecbede440fde47c65762c
Wilson <wilson.diant@gmail.com>
2024-12-25 22:45:35 -0500

    fix: Update import path for stopNames.json in TimelineItem component

--- alertsDisplayApp/src/pages/components/TimelineItem.js
+++ alertsDisplayApp/src/pages/components/TimelineItem.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import stopNames from '../../util/stopNames.json';
+import stopNames from '../../../util/stopNames.json';
 
 import { AccordionContext } from '../index.js';
 const TimelineItem = (props) => {

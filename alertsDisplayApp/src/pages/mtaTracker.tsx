3ff33e0f418b478279257ad7c842faae1ab60d03
Wilson <wilson.diant@gmail.com>
2025-03-05 19:30:34 -0500

    fix: Add JSX import to mtaTracker.tsx for type support

--- alertsDisplayApp/src/pages/mtaTracker.tsx
+++ alertsDisplayApp/src/pages/mtaTracker.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useState } from 'react';
+import { createContext, JSX, useCallback, useState } from 'react';
 import objects from '../../util/subwayLineColors.json';
 import EndMarker from '../components/EndMarker';
 import FilterControls from '../components/FilterControls';

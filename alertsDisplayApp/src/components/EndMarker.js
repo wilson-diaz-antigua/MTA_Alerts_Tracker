726c19eaa01b098d5424bb296b3804b87db695c8
Wilson <wilson.diant@gmail.com>
2025-03-14 18:08:30 -0400

    fix: Update height and viewBox of EndMarker component for improved display

--- alertsDisplayApp/src/components/EndMarker.js
+++ alertsDisplayApp/src/components/EndMarker.js
@@ -15,10 +15,10 @@ const EndMarker = ({ linecolors, objects }) => (
 			}`}
 		>
 			<svg
-				height={40}
+				height={50}
 				width={50}
 				xmlns='http://www.w3.org/2000/svg'
-				viewBox='0 0 60 60'
+				viewBox='10 0 40 40'
 			>
 				<rect width='60' height='20' x='0' y='0' rx='0' ry='0' />
 			</svg>

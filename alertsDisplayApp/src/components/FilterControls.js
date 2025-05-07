b515937991b26e25c97cd7b20935de42c330b9a5
Wilson <wilson.diant@gmail.com>
2025-05-07 14:34:51 -0400

    fix: Refactor FilterControls component for improved readability and consistency

--- alertsDisplayApp/src/components/FilterControls.js
+++ alertsDisplayApp/src/components/FilterControls.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types';
-import SummaryIcon from '../assets/summaryIcon';
-import terminal from '../constants/terminalDirections';
-import FilteredAlerts from './FilteredAlerts';
+import PropTypes from "prop-types";
+import SummaryIcon from "../assets/summaryIcon";
+import terminal from "../constants/terminalDirections";
+import FilteredAlerts from "./FilteredAlerts";
 
 /**
  * Filter Controls Component for MTA tracker
@@ -24,94 +24,94 @@ import FilteredAlerts from './FilteredAlerts';
  */
 
 const FilterControls = ({
-	stopNames,
-	setSummary,
-	summary,
-	homeStation,
-	setHomeStation,
-	filtLines,
-	setFiltLines,
-	service,
-	setService,
-	direction,
-	setDirection,
-	objects,
+  stopNames,
+  setSummary,
+  summary,
+  homeStation,
+  setHomeStation,
+  filtLines,
+  setFiltLines,
+  service,
+  setService,
+  direction,
+  setDirection,
+  objects,
 }) => (
-	<section className='flex pt-5 ml-[04rem] md:ml-[10.2rem] lg:ml-[12.5rem]'>
-		<div className='w-20 h-20 '>
-			<FilteredAlerts
-				data={objects.serviceByLines[filtLines]}
-				state={service}
-				setState={(e) => setService(e.target.value)}
-				className={`[text-align-last:center] pt-2 m-0.5 p-2.5 rounded-full w-full h-full mb-0 text-slate-50 font-bold text-6xl text-center ${objects.lineColors[filtLines]}`}
-				value={true}
-			/>
-		</div>
-		<div className='w-44 ml-2 flex flex-col'>
-			<div className=''>
-				<FilteredAlerts
-					setState={(e) => {
-						setHomeStation(e.target.value);
+  <section className="ml-[04rem] flex pt-5 md:ml-[10.2rem] lg:ml-[12.5rem]">
+    <div className="h-20 w-20">
+      <FilteredAlerts
+        data={objects.serviceByLines[filtLines]}
+        state={service}
+        setState={(e) => setService(e.target.value)}
+        className={`m-0.5 mb-0 h-full w-full rounded-full p-2.5 pt-2 text-center text-6xl font-bold text-slate-50 [text-align-last:center] ${objects.lineColors[filtLines]}`}
+        value={true}
+      />
+    </div>
+    <div className="ml-2 flex w-44 flex-col">
+      <div className="">
+        <FilteredAlerts
+          setState={(e) => {
+            setHomeStation(e.target.value);
 
-						setSummary(true);
-					}}
-					state={homeStation}
-					data={stopNames}
-					className=' h-auto w-36 iphone-14pro-max:w-48 md:w-64 truncate pt-1 text-lg font-black uppercase bg-transparent justify-self-start text-slate-50'
-					value={true}
-				/>
-			</div>
-			<div className=''>
-				<div className=' iphone-14pro-max:flex items-center justify-between'>
-					<FilteredAlerts
-						setState={(e) => {
-							setService(objects.serviceByLines[filtLines][0]);
-							setFiltLines(e.target.value);
-						}}
-						state={filtLines}
-						data={objects.serviceByLines}
-						className='content-center h-auto w-auto text-sm    iphone-14pro-max:text-right  font-bold uppercase bg-transparent text-zinc-400'
-						value={false}
-					/>
-					<span className='content-center iphone-14pro-max:mx-2 hidden  iphone-14pro-max:inline text-lg font-bold uppercase bg-transparent text-zinc-50'>
-						|
-					</span>
-					<FilteredAlerts
-						setState={(e) => {
-							setService(objects.serviceByLines[filtLines][0]);
-							setDirection(e.target.value);
-						}}
-						data={terminal}
-						state={direction}
-						className='content-center h-auto text-sm   font-bold uppercase bg-transparent justify-self-start text-zinc-400'
-						value={false}
-					/>
-				</div>
-			</div>
-		</div>
-		<div className=' iphone-14pro-max:ml-10 mr-4 flex items-center justify-end '>
-			<button
-				onClick={() => setSummary(!summary)}
-				className='  w-11 h-11 rounded-full flex items-center justify-center bg-zinc-600 md:hidden'
-			>
-				<SummaryIcon />
-			</button>
-		</div>
-	</section>
+            setSummary(true);
+          }}
+          state={homeStation}
+          data={stopNames}
+          className="h-auto w-36 justify-self-start truncate bg-transparent pt-1 text-lg font-black uppercase text-slate-50 iphone-14pro-max:w-48 md:w-64"
+          value={true}
+        />
+      </div>
+      <div className="">
+        <div className="items-center justify-between iphone-14pro-max:flex">
+          <FilteredAlerts
+            setState={(e) => {
+              setService(objects.serviceByLines[filtLines][0]);
+              setFiltLines(e.target.value);
+            }}
+            state={filtLines}
+            data={objects.serviceByLines}
+            className="h-auto w-auto content-center bg-transparent text-sm font-bold uppercase text-zinc-400 [text-align-last:right] iphone-14pro-max:text-right"
+            value={false}
+          />
+          <span className="hidden content-center bg-transparent text-lg font-bold uppercase text-zinc-50 iphone-14pro-max:mx-2 iphone-14pro-max:inline">
+            |
+          </span>
+          <FilteredAlerts
+            setState={(e) => {
+              setService(objects.serviceByLines[filtLines][0]);
+              setDirection(e.target.value);
+            }}
+            data={terminal}
+            state={direction}
+            className="h-auto content-center justify-self-start bg-transparent text-sm font-bold uppercase text-zinc-400"
+            value={false}
+          />
+        </div>
+      </div>
+    </div>
+    <div className="mr-4 flex items-center justify-end iphone-14pro-max:ml-10">
+      <button
+        onClick={() => setSummary(!summary)}
+        className="flex h-11 w-11 items-center justify-center rounded-full bg-zinc-600 md:hidden"
+      >
+        <SummaryIcon />
+      </button>
+    </div>
+  </section>
 );
 
 FilterControls.propTypes = {
-	stopNames: PropTypes.array,
+  stopNames: PropTypes.array,
 
-	filtLines: PropTypes.string.isRequired,
-	setFiltLines: PropTypes.func.isRequired,
-	homeStation: PropTypes.string.isRequired,
-	setHomeStation: PropTypes.func.isRequired,
-	service: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
-	setService: PropTypes.func.isRequired,
-	direction: PropTypes.string.isRequired,
-	setDirection: PropTypes.func.isRequired,
-	objects: PropTypes.object.isRequired,
+  filtLines: PropTypes.string.isRequired,
+  setFiltLines: PropTypes.func.isRequired,
+  homeStation: PropTypes.string.isRequired,
+  setHomeStation: PropTypes.func.isRequired,
+  service: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
+  setService: PropTypes.func.isRequired,
+  direction: PropTypes.string.isRequired,
+  setDirection: PropTypes.func.isRequired,
+  objects: PropTypes.object.isRequired,
 };
 
 export default FilterControls;
